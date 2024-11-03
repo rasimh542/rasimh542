034d64fa359b25bb01001f31da988491b6318c82
rasimh542 <98226218+rasimh542@users.noreply.github.com>
2024-11-03 11:33:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 # setup.py
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="your_package_name",
     version="0.1.0",
-    description="A brief description of your package",
     author="Your Name",
-    author_email="your.email@example.com",
-    packages=find_packages(),
+    author_email="your-email@example.com",
+    description="A short description of the package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # or "text/x-rst" if using reStructuredText
+    packages=["your_package_name"],
+    install_requires=[],
+    # Other metadata...
 )
