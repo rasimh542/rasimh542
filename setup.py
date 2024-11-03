2c57f97b0c1e56d5b02c783169fdf79e4598b196
rasimh542 <98226218+rasimh542@users.noreply.github.com>
2024-11-03 11:21:47 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="your_package_name",
+    version="0.1.0",
+    description="A brief description of your package",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+)
